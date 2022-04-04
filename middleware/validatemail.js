c12ddac09691e3e00c0593cdfe90eca65d0cded5
Jean Marcel <jeanvanzella91@gmail.com>
2022-04-04 01:55:55 -0300

    fixing req 3 email problem

--- middleware/validatemail.js
+++ middleware/validatemail.js
@@ -3,7 +3,7 @@ const validatemail = (req, res, next) => {
   if (!email || email === '') {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-  if (!email.includes('@')) {
+  if (!email.includes('@') || !email.includes('.com')) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
 
